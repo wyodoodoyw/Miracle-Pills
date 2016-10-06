5114db8b556cafb16cda988c662202f19a7945b9
wyodoodoyw <mwood051@gmail.com>
2016-10-06 11:46:30 -0400

    Picker Function
    Implemented Picker functionality

--- MiraclePills/ViewController.swift
+++ MiraclePills/ViewController.swift
@@ -12,12 +12,16 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     @IBOutlet weak var provincePickerButton: UIButton!
     @IBOutlet weak var provincePicker: UIPickerView!
     
+    var provinces = ["Newfoundland", "PEI", "Nova Scotia", "New Brunswick", "Québec", "Ontario", "Manitoba", "Saskatchewan", "Alberta", "British Columbia", "Yukon", "Northwest Territories", "Nunavut", "钓鱼岛"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         provincePicker.dataSource = self
         provincePicker.delegate = self
+        
+        provinces = provinces.sorted()
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,19 +30,25 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     }
 
     @IBAction func provinceButtonPressed(_ sender: UIButton) {
+        provincePicker.isHidden = false
     
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 0
+        return 1
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return 0
+        return provinces.count
     }
 
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return ""
+        return provinces[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        provincePickerButton.setTitle(provinces[row], for: UIControlState.normal)
+        provincePicker.isHidden = true
     }
 }
 
