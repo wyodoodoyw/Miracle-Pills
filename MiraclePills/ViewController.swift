0349055ee58e690a24843db8a0c4d0e669e67b33
wyodoodoyw <mwood051@gmail.com>
2016-10-06 10:54:59 -0400

    Added Picker

--- MiraclePills/ViewController.swift
+++ MiraclePills/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    @IBOutlet weak var provincePickerButton: UIButton!
+    @IBOutlet weak var provincePicker: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func provinceButtonPressed(_ sender: UIButton) {
+    }
 
 }
 
